fbc4d11ff411459a75a2c3625268db27a1c29c5f
navdeep278 <142869106+navdeep278@users.noreply.github.com>
2026-04-06 00:11:31 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -23,11 +23,9 @@ def load_model():
 
 @st.cache_resource
 def init_connection():
-    client = MongoClient(
-        client = MongoClient(st.secrets["MONGO_URI"])
-    )
+    client = MongoClient(st.secrets["MONGO_URI"])
     return client["printer_maintenance"]["sensor_data_ml"]
-
+    )
 
 # ============================================================
 # SECTION 3: DATA FETCHING & PREDICTION
